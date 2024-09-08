a962557282ae6f76aa2974fbf1762f531bb03d42
Nortey-Micheal <norteymicheal91@gmail.com>
2024-09-08 12:55:10 -0700

    feat: add functions and their corresponding tests

--- index.js
+++ index.js
@@ -2,4 +2,61 @@ function sum (a,b) {
     return a + b;
 }
 
-export {sum};
+function capitalize (word) {
+    let newWord = word.slice(1)
+    let capital = word[0].toUpperCase();
+    return capital + newWord;
+}
+
+function reverseString (word) {
+    let newString = ""
+    for (let index = word.length - 1; index >= 0; index--) {
+        newString += word[index]
+    }
+    return newString;
+}
+
+function calculator() {
+    const add = (a,b) => {
+        return a + b;
+    }
+
+    const subtract = (a,b) => {
+        return a - b;
+    }
+
+    const divide = (a,b) => {
+        return a / b;
+    }
+
+    const multiply = (a,b) => {
+        return a * b;
+    }
+
+    return {
+        add,
+        subtract,
+        divide,
+        multiply
+    }
+}
+
+function analyzeArray (array) {
+    let fx = 0;
+    for (let index = 0; index < array.length; index++) {
+        fx += array[index]
+    }
+    let average = fx / array.length;
+    let min = Math.min(...array);
+    let max = Math.max(...array);
+    let length = array.length;
+    
+    return {
+        "average": average,
+        "length": length,
+        "max": max,
+        "min": min
+    }
+}
+
+export {sum,capitalize,reverseString,calculator,analyzeArray};
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import {sum} from "./index.js";
+import {sum,capitalize,reverseString,calculator,analyzeArray} from "./index.js";
 
 test("Done",() => {
 
@@ -8,3 +8,35 @@ test("Add 1 + 2 to equal 3", () => {
     expect(sum(1,2)).toBe(3)
 })
 
+test("Capitalize a string", () => {
+    expect(capitalize("goat")).toBe("Goat")
+})
+
+test("Reverse a string", () => {
+    expect(reverseString("goat")).toBe("taog")
+})
+
+test("Calculator's add function", () => {
+    expect(calculator().add(4,2)).toBe(6);
+})
+
+test("Calculator's subtract function",() => {
+    expect(calculator().subtract(4,1)).toBe(3);
+})
+
+test("Calculator's divide function",() => {
+    expect(calculator().divide(4,2)).toBe(2);
+})
+
+test("Calculator's multiply function",() => {
+    expect(calculator().multiply(4,2)).toBe(8);
+})
+
+test("Analyzing the provided array", () => {
+    expect(analyzeArray([1,8,3,4,2,6])).toStrictEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+     });
+})
